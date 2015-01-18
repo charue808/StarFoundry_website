8d62563db91e3fd226834b2a908190f54c051b93
jayqueue <jay.queue47@gmail.com>
2015-01-18 06:23:59 +0000

    Make drill-down dynamic

--- js/concepts_taught.js
+++ js/concepts_taught.js
@@ -3,16 +3,12 @@
  * http://jsfiddle.net/zaheerahmed/7xEhW/
  * http://jsfiddle.net/KNFJE/36/
  */
-function destroyChart(container){
-    $(container).highcharts().destroy();
+function destroyChart(){
+    $('#concepts_taught_twt_container').highcharts().destroy();
 }
 
 
-
-
-
-
-function drawChart(container, conceptsObject){
+function drawChart(title){
     dataLabelStyle = 
         { 
         fontWeight: 'bold', 
@@ -20,10 +16,15 @@ function drawChart(container, conceptsObject){
         width: '150px'
     };
     
+    //DEBUG
+    //
+    console.log("Debug 1");
+    
      var $conceptReporting = $('#conceptReporting');
+     var conceptsObject = getCourseConcepts(title);
     
     // Create the chart
-    $(container).highcharts({
+    $('#concepts_taught_twt_container').highcharts({
         chart: {
             type: 'pie',
             backgroundColor:'rgba(255, 255, 255, 0.1)'
@@ -141,34 +142,57 @@ function drawChart(container, conceptsObject){
 
 
 function getCourseConcepts(courseTitle) {
-    var concepts = "";
+   
+    var conceptsObject = {};
+   
+            
+    // Flat Colors Source: http://www.flatuicolorpicker.com/
+
+    var businessColors = ['#4183D7', '#59ABE3 ', '#81CFE0 ', '#52B3D9', '#22A7F0', '#3A539B'];
+    var codingColors = ['#9A12B3', '#BF55EC', '#BE90D4', '#8E44AD', '#9B59B6', '#663399'];    
+    var designColors = ['#D24D57', '#F22613', '#D91E18', '#96281B', '#EF4836', '#D64541', '#C0392B'];
+
     
     switch(courseTitle) {
         case "Tinkering with Twitter":
-            description =   " <p>Dive right into the real world of building world-changing products, by \
-                            developing the version of the popular web tool, Twitter, from the ground up. \
-                            </p>\
-                            <p>This version of Twitter was so successful, it is now one of the top 10 most used sites in the world.</p> \
-                            <p>This hands-on workshop will give students familiarity with \"rapid prototyping\" concepts, which are most concerned \
-                            with how to launch quickly, efficiently, and effectively. We will explore topics such as web tools, visual design, \
-                            managing data in the cloud, creating user engagement and interactivity through animations and javascript, customizing \
-                            user experiences, and keeping your application data safe.</p>";
+            tinkeringWithTwitterConcepts = {};
 
+            tinkeringWithTwitterConcepts.business = [
+                {name:'Business Unit 1: Identifying Your Value Proposition', y:3, color: businessColors[0]},
+                {name:'Business Unit 2: Marketing Your Product', y: 2, color: businessColors[1] }
+            ];
+
+            tinkeringWithTwitterConcepts.coding = [              
+                {name:'Coding Unit 1: Leveraging Modern Mock-Up Tools', y:1, color: codingColors[0]},
+                {name:'Coding Unit 2: Launching a live site', y:3, color: codingColors[1]},
+                {name:'Coding Unit 3: Working with web visuals', y:1, color: codingColors[2]},
+                {name:'Coding Unit 4: Implementing user accounts systems', y:4, color: codingColors[3]},
+                {name:'Coding Unit 5: Storing, retrieving, and displaying database data', y:7, color: codingColors[4]},
+            ];
+
+
+            tinkeringWithTwitterConcepts.design = [
+                {name:'Design Unit 1: Basic Mock-Up Principles', y:1, color: designColors[0]},
+                {name:'Design Unit 2: Managing user experiences', y:3, color: designColors[1]},
+                {name:'Design Unit 3: Beautifying with bootstrap', y:2, color: designColors[2]},
+            ];
+
+            conceptsObject = tinkeringWithTwitterConcepts;
             break;
         case "Rapid Prototyping Fundamentals" :
-            description =   " ....."
+            conceptsObject = {};
             break;
         case "Rapid Prototyping for Developers" :
-            description =   " ....."
+            conceptsObject = {};
             break;
         case "Rapid Prototyping for MBAs" :
-            description =   "....."
+            conceptsObject = {};
             break;
         case "Rapid Prototyping for Intrapreneurs" :
-            description =   "....."
+            conceptsObject = {};
             break;
         case "Global Dreamers Institute" :
-            description =   "....."
+            conceptsObject = {};
             break;
         default:
             description = "course title not found";
@@ -178,7 +202,7 @@ function getCourseConcepts(courseTitle) {
  //   console.log(courseTitle);
  //   console.log("Tinkering with Twitter");
  
-    return description;
+    return conceptsObject;
 
 }
 
@@ -199,45 +223,6 @@ $(function () {
      colors: ['#446CB3', '#674172', '#DB0A5B']
     });
   
-    // Flat Colors Source: http://www.flatuicolorpicker.com/
-    
-    var businessColors = ['#4183D7', '#59ABE3 ', '#81CFE0 ', '#52B3D9', '#22A7F0', '#3A539B'];
-    var codingColors = ['#9A12B3', '#BF55EC', '#BE90D4', '#8E44AD', '#9B59B6', '#663399'];    
-    var designColors = ['#D24D57', '#F22613', '#D91E18', '#96281B', '#EF4836', '#D64541', '#C0392B'];
-    
-    tinkeringWithTwitterConcepts = {};
 
-    tinkeringWithTwitterConcepts.business = [
-                    {name:'Business Unit 1: Identifying Your Value Proposition', y:3, color: businessColors[0]},
-                    {name:'Business Unit 2: Marketing Your Product', y: 2, color: businessColors[1] }
-                ];
-    
-    tinkeringWithTwitterConcepts.coding = [              
-                    {name:'Coding Unit 1: Leveraging Modern Mock-Up Tools', y:1, color: codingColors[0]},
-                    {name:'Coding Unit 2: Launching a live site', y:3, color: codingColors[1]},
-                    {name:'Coding Unit 3: Working with web visuals', y:1, color: codingColors[2]},
-                    {name:'Coding Unit 4: Implementing user accounts systems', y:4, color: codingColors[3]},
-                    {name:'Coding Unit 5: Storing, retrieving, and displaying database data', y:7, color: codingColors[4]},
-                ];
-    
-    
-    tinkeringWithTwitterConcepts.design = [
-                    {name:'Design Unit 1: Basic Mock-Up Principles', y:1, color: designColors[0]},
-                    {name:'Design Unit 2: Managing user experiences', y:3, color: designColors[1]},
-                    {name:'Design Unit 3: Beautifying with bootstrap', y:2, color: designColors[2]},
-                ];
-    
-
-    // To activate a chart when a page loads, make two calls like the draw and destroy functions
-    // below for each modal to have a chart drawn on it:
-    // 
-    $('#courseInfoModal').on('shown.bs.modal', function (e) {
-       drawChart('#concepts_taught_twt_container', tinkeringWithTwitterConcepts);
-    })
-    
-    $('#courseInfoModal').on('hidden.bs.modal', function (e) {
-       destroyChart('#concepts_taught_twt_container');
-    })
-    
     
 });
