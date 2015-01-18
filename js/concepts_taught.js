c84230a52990cee3b777e88b50680c70345c6146
jayqueue <jay.queue47@gmail.com>
2015-01-18 05:57:09 +0000

    Course descriptions dynamic

--- js/concepts_taught.js
+++ js/concepts_taught.js
@@ -136,6 +136,57 @@ function drawChart(container, conceptsObject){
     });
 }
 
+
+
+
+
+function getCourseConcepts(courseTitle) {
+    var concepts = "";
+    
+    switch(courseTitle) {
+        case "Tinkering with Twitter":
+            description =   " <p>Dive right into the real world of building world-changing products, by \
+                            developing the version of the popular web tool, Twitter, from the ground up. \
+                            </p>\
+                            <p>This version of Twitter was so successful, it is now one of the top 10 most used sites in the world.</p> \
+                            <p>This hands-on workshop will give students familiarity with \"rapid prototyping\" concepts, which are most concerned \
+                            with how to launch quickly, efficiently, and effectively. We will explore topics such as web tools, visual design, \
+                            managing data in the cloud, creating user engagement and interactivity through animations and javascript, customizing \
+                            user experiences, and keeping your application data safe.</p>";
+
+            break;
+        case "Rapid Prototyping Fundamentals" :
+            description =   " ....."
+            break;
+        case "Rapid Prototyping for Developers" :
+            description =   " ....."
+            break;
+        case "Rapid Prototyping for MBAs" :
+            description =   "....."
+            break;
+        case "Rapid Prototyping for Intrapreneurs" :
+            description =   "....."
+            break;
+        case "Global Dreamers Institute" :
+            description =   "....."
+            break;
+        default:
+            description = "course title not found";
+    }
+    
+ // DEBUG   
+ //   console.log(courseTitle);
+ //   console.log("Tinkering with Twitter");
+ 
+    return description;
+
+}
+
+
+
+
+
+
 // This is run on page load
 //
 $(function () {
@@ -153,8 +204,6 @@ $(function () {
     var businessColors = ['#4183D7', '#59ABE3 ', '#81CFE0 ', '#52B3D9', '#22A7F0', '#3A539B'];
     var codingColors = ['#9A12B3', '#BF55EC', '#BE90D4', '#8E44AD', '#9B59B6', '#663399'];    
     var designColors = ['#D24D57', '#F22613', '#D91E18', '#96281B', '#EF4836', '#D64541', '#C0392B'];
-
-    //var colorArrays = [businessColors, codingColors, designColors];
     
     tinkeringWithTwitterConcepts = {};
 
---
+++ js/course_descriptions.js
@@ -0,0 +1,51 @@
+/**
+* Created with Merge January 17.
+* User: jayqueue
+* Date: 2015-01-18
+* Time: 05:04 AM
+* To change this template use Tools | Templates.
+*/
+
+
+function getCourseDescription(courseTitle) {
+    var description = "";
+    
+    switch(courseTitle) {
+        case "Tinkering with Twitter":
+            description =   " <p>Dive right into the real world of building world-changing products, by \
+                            developing the version of the popular web tool, Twitter, from the ground up. \
+                            </p>\
+                            <p>This version of Twitter was so successful, it is now one of the top 10 most used sites in the world.</p> \
+                            <p>This hands-on workshop will give students familiarity with \"rapid prototyping\" concepts, which are most concerned \
+                            with how to launch quickly, efficiently, and effectively. We will explore topics such as web tools, visual design, \
+                            managing data in the cloud, creating user engagement and interactivity through animations and javascript, customizing \
+                            user experiences, and keeping your application data safe.</p>";
+
+            break;
+        case "Rapid Prototyping Fundamentals" :
+            description =   " ....."
+            break;
+        case "Rapid Prototyping for Developers" :
+            description =   " ....."
+            break;
+        case "Rapid Prototyping for MBAs" :
+            description =   "....."
+            break;
+        case "Rapid Prototyping for Intrapreneurs" :
+            description =   "....."
+            break;
+        case "Global Dreamers Institute" :
+            description =   "....."
+            break;
+        default:
+            description = "course title not found";
+    }
+    
+ // DEBUG   
+ //   console.log(courseTitle);
+ //   console.log("Tinkering with Twitter");
+ 
+    return description;
+
+}
+
