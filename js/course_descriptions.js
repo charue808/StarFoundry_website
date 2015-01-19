4729effeca1eb5172c5a9536052b72116b889137
charue808 <charlesgum@gmail.com>
2015-01-19 22:14:51 +0000

    add in new couse to course description js, broke modal

--- js/course_descriptions.js
+++ js/course_descriptions.js
@@ -21,8 +21,20 @@ function getCourseDescription(courseTitle) {
                             user experiences, and keeping your application data safe.</p>";
 
             break;
-        case "Rapid Prototyping Fundamentals" :
-            description =   " ....."
+        case "Turn Ideas into Real Apps Fast":
+            description =   " <p>In just 12 weeks, you'll learn how to turn a dream into your very own web \
+                              product. You'll gain the skills to build an early version of the world changing web \
+                              application, Facebook, from scratch. This version was successful it grew \
+                              into the 2nd most visited site on earth today. </p> \
+                              <p>Along the way we'll build the skills to launch the three major pieces of just about \
+                              every web solution we use: <b>collecting</b>, <b>processing</b>, and <b>presenting data</b> \
+                              (AirBnB, for example, <b>collects</b> client preference and location information,\
+                              <b>processes</b> it to figure out which locations match with which clients preferences, \
+                              and <b>presents</b> those findings to clients looking for a place to stay).</p>  \
+                              <p>We'll even show you how to quickly solve the design challenges inherent to any launch, \
+                              while also giving you the key business tools to reach and keep your customers. </p> \
+                              <p><b>Minimal requirements: We ask that all students first attend our free <a href="">Tinkering with Twitter</a> \
+                              course, and bring a wireless internet-enabled device with a keyboard.</p>";
             break;
         case "Rapid Prototyping for Developers" :
             description =   " ....."
