cb09a5b66e9615bc7c2794820f751fd0addc68f9
charue808 <charlesgum@gmail.com>
2015-01-20 10:01:51 +0000

    form reset js created, added form reset to form class, form 1014 and form 1015, tested ok

---
+++ js/lib/form_reset.js
@@ -0,0 +1,13 @@
+/**
+* Created with StarFoundry_website.
+* User: charue808
+* Date: 2015-01-20
+* Time: 09:42 AM
+* To change this template use Tools | Templates.
+*/
+
+$(document).ready(function() {
+      $('.form')[0].reset();
+      $('#_form_1013')[0].reset();
+      $('#_form_1015')[0].reset();
+});
