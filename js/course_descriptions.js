5113a64383af3549335ec7388057b75b3c56b985
charue808 <charlesgum@gmail.com>
2015-01-20 02:27:29 +0000

    edited to make tinkering with twitter link in 2nd course work, tested ok

--- js/course_descriptions.js
+++ js/course_descriptions.js
@@ -33,7 +33,7 @@ function getCourseDescription(courseTitle) {
                               and <b>presents</b> those findings to clients looking for a place to stay).</p>  \
                               <p>We'll even show you how to quickly solve the design challenges inherent to any launch, \
                               while also giving you the key business tools to reach and keep your customers. </p> \
-                              <p><b>Minimal requirements:</b> We ask that all students first attend our <b>free</b> Tinkering with Twitter \
+                              <p><b>Minimal requirements:</b> We ask that all students first attend our free <a target='_blank' href='http://nelson-common.codio.io/#courses'>Tinkering with Twitter</a> \
                               course, and bring a wireless internet-enabled device with a keyboard.</p>";
             break;
         case "Rapid Prototyping for Developers" :
